359f29d843c001acf20490e92925aae05ffed980
hyeonjeong <phj051213@gmail.com>
2026-05-28 02:33:19 +0900

    seo: 네이버 서치어드바이저 인증을 라우트 핸들러로 우회 처리
    - app/[displayName] 동적 라우트가 public 폴더의 정적 html 파일을 가로채는(Intercept) 문제를 해결하기 위해, 명시적으로 해당 URL의 Route Handler(route.ts)를 생성하여 100% 우선순위로 응답하도록 수정

---
+++ app/naver98bdaf1747bc4e99deccb091ed53e0f8.html/route.ts
@@ -0,0 +1,7 @@
+export function GET() {
+  return new Response("naver-site-verification: naver98bdaf1747bc4e99deccb091ed53e0f8.html", {
+    headers: {
+      "Content-Type": "text/html; charset=utf-8",
+    },
+  });
+}
